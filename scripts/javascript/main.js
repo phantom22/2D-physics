62a8cf73777b7c2828b6bcc5190d2218ff8c6262
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2022-04-05 16:08:58 +0200

    Add files via upload

--- scripts/javascript/main.js
+++ scripts/javascript/main.js
@@ -1,57 +1,84 @@
 ;
-function _(x, y) {
-    return new Vector2D(x, y);
-}
-class Vector2D {
+class Vector2 {
+    static up() {
+        return new Vector2(0, 1);
+    }
+    static down() {
+        return new Vector2(0, -1);
+    }
+    static left() {
+        return new Vector2(-1, 0);
+    }
+    static right() {
+        return new Vector2(1, 0);
+    }
+    static one() {
+        return new Vector2(1, 1);
+    }
+    static zero() {
+        return new Vector2(0, 0);
+    }
+    toString() {
+        return `Vector2(${this.x},${this.y})`;
+    }
+    get 0() {
+        return this.x;
+    }
+    get 1() {
+        return this.y;
+    }
+    equals(v) {
+        return this === v;
+    }
+    clone() {
+        return new Vector2(this.x, this.y);
+    }
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-    vec() {
-        return [this.x, this.y];
-    }
-    sum(v) {
-        this.x += v.x;
-        this.y += v.y;
+    add(v) {
+        return new Vector2(this.x + v.x, this.y + v.y);
     }
     sub(v) {
-        this.x -= v.x;
-        this.y -= v.y;
+        return new Vector2(this.x - v.x, this.y - v.y);
     }
     mul(n) {
-        this.x *= n;
-        this.y *= n;
-    }
-    div(n) {
-        this.x /= n;
-        this.y /= n;
-    }
-    _sum(v) {
-        return _(this.x + v.x, this.y + v.y);
-    }
-    _sub(v) {
-        return _(this.x - v.x, this.y - v.y);
-    }
-    _mul(n) {
-        return _(this.x * n, this.y * n);
-    }
-    _div(n) {
-        return _(this.x / n, this.y / n);
+        return new Vector2(this.x * n, this.y * n);
     }
     dot(v) {
         return this.x * v.x + this.y * v.y;
     }
-    dist(v) {
-        return Math.sqrt((this.x - v.x) ** 2 + (this.y - v.y) ** 2);
+    scale(v) {
+        return new Vector2(this.x * v.x, this.y * v.y);
     }
-    length() {
+    div(n) {
+        return new Vector2(this.x / n, this.y / n);
+    }
+    magnitude() {
         return Math.sqrt(this.x ** 2 + this.y ** 2);
     }
-    dir() {
-        const len = this.length();
-        return [this.x / len, this.y / len];
+    sqrMagnitude() {
+        return this.x ** 2 + this.y ** 2;
+    }
+    normalize() {
+        const len = this.magnitude();
+        return new Vector2(this.x / len, this.y / len);
+    }
+    distance(v) {
+        return Math.sqrt((this.x - v.x) ** 2 + (this.y - v.y) ** 2);
+    }
+    angle(v) {
+        return Math.acos(this.dot(v) / (this.magnitude() * v.magnitude()));
+    }
+    clamp(x, y) {
+        return new Vector2(Math.max(x[0], Math.min(this.x, x[1])), Math.max(y[0], Math.min(this.y, y[1])));
     }
 }
+const Physics = {
+    gravitationalConstant: 6.674E-11,
+    fps: 60
+};
 class Planet {
     constructor(pos, radius, mass, dir, col) {
         this.pos = pos;
@@ -60,13 +87,42 @@ class Planet {
         this.dir = dir;
         this.col = col;
     }
-    update() {
-        this.pos.sum(this.dir._div(60));
+    equals(p) {
+        return this === p;
     }
-    calcPull(p, scale) {
-        const dir = p.pos._sub(this.pos);
-        const x = 6.674E-11 * ((this.mass * p.mass) / (this.pos.x - p.pos.x) ** 2) * (dir.x / dir.length()), y = 6.674E-11 * ((this.mass * p.mass) / (this.pos.y - p.pos.y) ** 2) * (dir.y / dir.length());
-        return _(x, y)._mul(scale);
+    applyPull(p, scale) {
+        const dir = p.pos.sub(this.pos).normalize(), G = Physics.gravitationalConstant;
+        const dist = this.pos.distance(p.pos);
+        const masses = Vector2.one().mul(this.mass * p.mass);
+        let pull = masses.div(dist);
+        pull = pull.mul(G * scale).scale(dir);
+        this.dir = this.dir.add(pull);
+    }
+    checkCollisions(p) {
+        const dist = this.pos.distance(p.pos) || 1, radii_sum = this.radius + p.radius;
+        if (dist <= radii_sum) {
+            //p.bounce();
+            this.pos = new Vector2(p.pos.x + (radii_sum + 1) * ((this.pos.x - p.pos.x) / dist), p.pos.y + (radii_sum + 1) * ((this.pos.y - p.pos.y) / dist));
+            const t = this.dir.clone();
+            this.dir = p.dir; //this.dir.sub(p.dir);
+            p.dir = t; //p.dir.sub(t);
+            return false;
+        }
+        return true;
+    }
+    update() {
+        const width = window.innerWidth, height = window.innerHeight;
+        const force = this.dir.div(Physics.fps);
+        this.pos = this.pos.add(force);
+        const { x, y } = this.pos, r = this.radius;
+        let correctedPosition = this.pos.clamp([0, width - r], [0, height - r]);
+        if (x - r <= 0 || x + r >= width) {
+            this.dir = this.dir.scale(new Vector2(-0.5, 1));
+        }
+        if (y - r <= 0 || y + r >= height) {
+            this.dir = this.dir.scale(new Vector2(1, -0.5));
+        }
+        this.pos = correctedPosition;
     }
 }
 class SolarSystem {
@@ -79,33 +135,35 @@ class SolarSystem {
         this.planets = planets;
         this.scale = scale;
     }
+    drawPlanet(p) {
+        const ctx = this.ctx;
+        ctx.beginPath();
+        ctx.arc(p.pos.x, p.pos.y, p.radius, 0, Math.PI * 2);
+        ctx.fillStyle = p.col;
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = p.col;
+        ctx.stroke();
+    }
     updateCanvas() {
         const canvas = this.canvas, ctx = this.ctx, pl = this.planets;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for (let p = 0; p < pl.length; p++) {
-            const c = pl[p];
-            c.update();
-            ctx.beginPath();
-            ctx.arc(c.pos.x, c.pos.y, c.radius, 0, Math.PI * 2);
-            ctx.fillStyle = c.col;
-            ctx.fill();
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = c.col;
-            ctx.stroke();
+        for (let i = 0; i < pl.length; i++) {
+            this.drawPlanet(pl[i]);
         }
     }
     physicsStep() {
         const pl = this.planets;
         for (let a = 0; a < pl.length; a++) {
-            let pull = _(0, 0);
+            pl[a].update();
             for (let b = 0; b < pl.length; b++) {
                 if (b === a)
                     continue;
-                pull.sum(pl[a].calcPull(pl[b], this.scale));
+                pl[a].applyPull(pl[b], this.scale);
+                pl[a].checkCollisions(pl[b]);
             }
-            pl[a].dir.sum(pull);
         }
         this.updateCanvas();
         requestAnimationFrame(this.physicsStep.bind(this));
--- scripts/javascript/test.js
+++ scripts/javascript/test.js
@@ -1,7 +1,68 @@
-const system = new SolarSystem("screen", 0.001, [ 
-    new Planet(_(200,200), 10, 1E10, _(0,0), "black"),
-    new Planet(_(400,350), 20, 1E8, _(0,0), "red"),
-    new Planet(_(650,450), 100, 1E5, _(0,0), "lightcoral")
-]);
+function _(x,y) {
+    return new Vector2(x,y);
+}
+
+let planets = [];
+const numOfPlanets = 128,
+      radius = 1,
+      mass = 1E7,
+      slice = Math.PI * 2 / numOfPlanets,
+      colors = ["#4FBDBA","#4FBDBA","#35858B"];
+
+let distFromCenter = 300,
+    currentColor = 0;
+
+const center = [window.innerWidth / 2, window.innerHeight / 2];
+for (let i=0; i<numOfPlanets; i++) {
+    const s = slice*i;
+    distFromCenter -= 0;
+    currentColor++;
+    currentColor%=colors.length;
+    planets.push(new Planet(
+        _(center[0]+distFromCenter*Math.cos(s), center[1]+distFromCenter*Math.sin(s)),
+        radius,
+        mass,
+        _(Math.cos(s)*-100*Math.sin(s)**1/3,Math.sin(s)*-100*Math.cos(s)**1/3),
+        colors[currentColor]
+    ));
+}
+
+planets.push(new Planet(
+    _(center[0], center[1]),
+    30,
+    1E10,
+    _(0,0),
+    "white"
+))
+
+const system = new SolarSystem("screen", 0.0001, planets);
+
+// const system = new SolarSystem("screen", 0.005, [
+//     new Planet(_(center[0] - center[0]/3, center[1] - center[1]/4),
+//     20,
+//     1E7,
+//     _(0,250),
+//     "#4FBDBA"),
+//     new Planet(_(center[0] + center[0]/3, center[1] - center[1]/4),
+//     20,
+//     1E7,
+//     _(-250,0),
+//     "#4FBDBA"),
+//     new Planet(_(center[0], center[1]),
+//     50,
+//     1E6,
+//     _(0,0),
+//     "#35858B"),
+//     new Planet(_(center[0] + center[0]/3, center[1] + center[1]/4),
+//     20,
+//     1E7,
+//     _(0,-250),
+//     "#072227"),
+//     new Planet(_(center[0] - center[0]/3, center[1] + center[1]/4),
+//     20,
+//     1E7,
+//     _(250,0),
+//     "#072227"),
+// ]);
 
 system.physicsStep();
